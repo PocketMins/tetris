7c8368bd69a1f637e019a02a0015410e89e91882
Jeffrey Hsiung <jeffrey@bearlybits.com>
2020-09-03 22:58:54 -0700

    Update shiftRowsAboveDown to use splice and unshift.

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
 const tetrisBoard = new Array(20);
 const framesPerSecond = 30;
-let longBlockX = 3;
-let longBlockY = 0;
+let blockX;
+let blockY;
 let currentFrame = 0;
 
 for (let i = 0; i < tetrisBoard.length; i++) {
   tetrisBoard[i] = new Array(10).fill(false);
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener('keydown', keyDownHandler, false);
 
 function keyDownHandler(e) {
-  if (e.key == "Left" || e.key == "ArrowLeft") {
+  if (e.key == 'Left' || e.key == 'ArrowLeft') {
     if (canBlockMoveLeft()) {
       moveBlockLeft();
     }
-  } else if (e.key == "Right" || e.key == "ArrowRight") {
+  } else if (e.key == 'Right' || e.key == 'ArrowRight') {
     if (canBlockMoveRight()) {
       moveBlockRight();
     }
-  } else if (e.key == "Down" || e.key == "ArrowDown") {
+  } else if (e.key == 'Down' || e.key == 'ArrowDown') {
     moveBlockDown();
   }
 }
 
 const moveBlockLeft = () => {
   setBlock(false);
-  longBlockX = longBlockX - 1;
+  blockX = blockX - 1;
   setBlock(true);
 };
 
 const moveBlockRight = () => {
   setBlock(false);
-  longBlockX = longBlockX + 1;
+  blockX = blockX + 1;
   setBlock(true);
 };
 
 const canBlockMoveRight = () => {
-  return longBlockX < 9 && !tetrisBoard[longBlockY][longBlockX + 1];
+  return blockX < 9 && !tetrisBoard[blockY][blockX + 1];
 };
 
 const canBlockMoveLeft = () => {
-  return longBlockX > 0 && !tetrisBoard[longBlockY][longBlockX - 1];
+  return blockX > 0 && !tetrisBoard[blockY][blockX - 1];
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  setBlock(true);
+document.addEventListener('DOMContentLoaded', () => {
+  createNewBlock();
   setInterval(function() {
     currentFrame++;
     if (currentFrame === 30) {
       currentFrame = 0;
-      if (isBlockAtBottom()) {
-        createNewBlock();
-      } else {
-        moveBlockDown();
-      }
+      moveBlockDown();
     }
     clearCompletedRows();
     drawBoard();
@@ -62,22 +58,22 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const isBlockAtBottom = () => {
-  return longBlockY === 19 || tetrisBoard[longBlockY + 1][longBlockX];
+  return blockY === 19 || tetrisBoard[blockY + 1][blockX];
 };
 
 function drawBoard() {
-  const tetrisTable = document.getElementById("TetrisTable");
-  let table = "";
+  const tetrisTable = document.getElementById('TetrisTable');
+  let table = '';
   for (let i = 0; i < tetrisBoard.length; i++) {
-    table = table + "<tr>";
+    table = table + '<tr>';
     for (let j = 0; j < tetrisBoard[i].length; j++) {
       if (tetrisBoard[i][j] === true) {
         table = table + '<td style="background-color: blue;"></td>';
       } else {
-        table = table + "<td></td>";
+        table = table + '<td></td>';
       }
     }
-    table = table + "</tr>";
+    table = table + '</tr>';
   }
   tetrisTable.innerHTML = table;
 }
@@ -87,20 +83,20 @@ function moveBlockDown() {
     createNewBlock();
   } else {
     setBlock(false);
-    longBlockY = longBlockY + 1;
+    blockY = blockY + 1;
     setBlock(true);
   }
 }
 
 const createNewBlock = () => {
-  longBlockX = 3;
-  longBlockY = 0;
+  blockX = 3;
+  blockY = 0;
   setBlock(true);
   currentFrame = 0;
 };
 
 const setBlock = value => {
-  tetrisBoard[longBlockY][longBlockX] = value;
+  tetrisBoard[blockY][blockX] = value;
 };
 
 const clearCompletedRows = () => {
@@ -122,8 +118,6 @@ const isRowComplete = rowNumber => {
 };
 
 const shiftRowsAboveDown = rowNumber => {
-  for (let i = rowNumber; i > 0; i--) {
-    tetrisBoard[i] = tetrisBoard[i - 1];
-  }
-  tetrisBoard[0] = new Array(10).fill(false);
+  tetrisBoard.splice(rowNumber, 1);
+  tetrisBoard.unshift(new Array(10).fill(false));
 };
