acf3c22ad86f01ecb70428cd1415bc82b8cf0bb3
Jeffrey Hsiung <jeffrey@bearlybits.com>
2020-09-07 22:29:42 -0700

    Adding support for long block and t block

--- main.js
+++ main.js
@@ -1,51 +1,117 @@
-const tetrisBoard = new Array(20);
-const framesPerSecond = 30;
-let blockX;
-let blockY;
-let currentFrame = 0;
+class Block {
+  x;
+  y;
+  type;
+  rotation;
+  board;
 
-for (let i = 0; i < tetrisBoard.length; i++) {
-  tetrisBoard[i] = new Array(10).fill(false);
+  boxPositions = {
+    long: [
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 0, y: 3 }
+    ],
+    t: [
+      { x: 0, y: 0 },
+      { x: -1, y: 0 },
+      { x: 1, y: 0 },
+      { x: 0, y: 1 }
+    ]
+  };
+
+  constructor(board, x, y, type = 'long', rotation = 'down') {
+    this.board = board;
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.rotation;
+  }
+
+  getAllBoxes() {
+    return this.boxPositions[this.type].map(position => ({
+      x: this.x + position.x,
+      y: this.y + position.y
+    }));
+  }
+
+  isAtBottom() {
+    const boxes = this.getAllBoxes();
+    return (
+      !!boxes.find(box => box.y === this.board.length - 1) ||
+      !!boxes.find(box => this.board[box.y + 1][box.x])
+    );
+  }
+
+  moveLeft() {
+    this.x--;
+  }
+
+  moveRight() {
+    this.x++;
+  }
+
+  moveDown() {
+    this.y++;
+  }
+
+  canMoveRight() {
+    const boxes = this.getAllBoxes();
+    return (
+      !boxes.find(box => box.x === 9) &&
+      !boxes.find(box => this.board[box.y][box.x + 1])
+    );
+  }
+
+  canMoveLeft() {
+    const boxes = this.getAllBoxes();
+    return (
+      !boxes.find(box => box.x === 0) &&
+      !boxes.find(box => this.board[box.y][box.x - 1])
+    );
+  }
+
+  move(y, x) {
+    this.y = y;
+    this.x = x;
+  }
 }
+
+const initBoard = () => {
+  const board = new Array(20);
+  for (let i = 0; i < board.length; i++) {
+    board[i] = new Array(10).fill(false);
+  }
+  return board;
+};
+
+const board = initBoard();
+const framesPerSecond = 30;
+let currentFrame = 0;
+let currentBlock;
 
 document.addEventListener('keydown', keyDownHandler, false);
 
+const addToBoard = block => {
+  const boxes = block.getAllBoxes();
+  for (let box of boxes) {
+    board[box.y][box.x] = true;
+  }
+};
+
 function keyDownHandler(e) {
   if (e.key == 'Left' || e.key == 'ArrowLeft') {
-    if (canBlockMoveLeft()) {
-      moveBlockLeft();
+    if (currentBlock.canMoveLeft()) {
+      currentBlock.moveLeft();
     }
   } else if (e.key == 'Right' || e.key == 'ArrowRight') {
-    if (canBlockMoveRight()) {
-      moveBlockRight();
+    if (currentBlock.canMoveRight()) {
+      currentBlock.moveRight();
     }
   } else if (e.key == 'Down' || e.key == 'ArrowDown') {
     moveBlockDown();
   }
 }
-
-const moveBlockLeft = () => {
-  moveBlock(blockY, blockX - 1);
-};
-
-const moveBlockRight = () => {
-  moveBlock(blockY, blockX + 1);
-};
-
-const moveBlock = (y, x) => {
-  setBlock(false);
-  blockX = x;
-  blockY = y;
-  setBlock(true);
-};
-
-const canBlockMoveRight = () => {
-  return blockX < 9 && !tetrisBoard[blockY][blockX + 1];
-};
-
-const canBlockMoveLeft = () => {
-  return blockX > 0 && !tetrisBoard[blockY][blockX - 1];
-};
 
 document.addEventListener('DOMContentLoaded', () => {
   createNewBlock();
@@ -60,17 +126,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 1000 / framesPerSecond);
 });
 
-const isBlockAtBottom = () => {
-  return blockY === 19 || tetrisBoard[blockY + 1][blockX];
-};
+function hasBlock(y, x) {
+  const allCurrentBlockBoxes = currentBlock.getAllBoxes();
+  return !!allCurrentBlockBoxes.find(box => box.y === y && box.x === x);
+}
 
 function drawBoard() {
   const tetrisTable = document.getElementById('TetrisTable');
   let table = '';
-  for (let i = 0; i < tetrisBoard.length; i++) {
+  for (let i = 0; i < board.length; i++) {
     table = table + '<tr>';
-    for (let j = 0; j < tetrisBoard[i].length; j++) {
-      if (tetrisBoard[i][j] === true) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (hasBlock(i, j)) {
+        table = table + '<td style="background-color: red;"></td>';
+      } else if (board[i][j] === true) {
         table = table + '<td style="background-color: blue;"></td>';
       } else {
         table = table + '<td></td>';
@@ -82,22 +151,17 @@ function drawBoard() {
 }
 
 function moveBlockDown() {
-  if (isBlockAtBottom()) {
+  if (currentBlock.isAtBottom()) {
+    addToBoard(currentBlock);
     createNewBlock();
   } else {
-    moveBlock(blockY + 1, blockX);
+    currentBlock.moveDown();
   }
 }
 
 const createNewBlock = () => {
-  blockX = 3;
-  blockY = 0;
-  setBlock(true);
+  currentBlock = new Block(board, 3, 0);
   currentFrame = 0;
-};
-
-const setBlock = value => {
-  tetrisBoard[blockY][blockX] = value;
 };
 
 const clearCompletedRows = () => {
@@ -109,7 +173,7 @@ const clearCompletedRows = () => {
 };
 
 const isRowComplete = rowNumber => {
-  const row = tetrisBoard[rowNumber];
+  const row = board[rowNumber];
   for (let i = 0; i < row.length; i++) {
     if (!row[i]) {
       return false;
@@ -119,6 +183,6 @@ const isRowComplete = rowNumber => {
 };
 
 const shiftRowsAboveDown = rowNumber => {
-  tetrisBoard.splice(rowNumber, 1);
-  tetrisBoard.unshift(new Array(10).fill(false));
+  board.splice(rowNumber, 1);
+  board.unshift(new Array(10).fill(false));
 };
